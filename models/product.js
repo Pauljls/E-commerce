a783af9f08c1b9b1226283d5358e8f19247a93de
Pauljls <paulazarsol@gmail.com>
2024-02-02 02:24:00 -0500

    detallando el esquema de products

--- models/product.js
+++ models/product.js
@@ -2,14 +2,79 @@ const mongoose  = require('mongoose')
 
 //PARA CREAR UN MODELO NECESITAMOS CREAR EL ESQUEMA
 const productSchema = mongoose.Schema({
-    name : String,
-    image: String,
-    //SI QUEREMOS AHCER UN ITEM REQUERIDO O DE LLENADO OBLIGATORIO
-    //SERA DE LA SIGUIENTE MANERA
-    countInStock : {
+    name : {
+        type : String,
+        required : true,
+
+    },
+    descripcionn : {
+        type: String,
+        required : true,
+
+    },
+    richDescription : {
+        type : String,
+        default : ''
+    },
+    image: {
+        type : String,
+        default : ' '
+    },
+    //DE ESTA FORMA DEFINIMOS QUE UN ITEM SERA UN ARRAY, 
+    //POR DENTRO IRA LA FORMA DEL OBJETO QUE ESTAMOS CREANO
+    //EN ESTE CASO UN ARRAY DE STRINGS
+    images : [{
+        type : String,
+
+    }],
+    brand : {
         type : Number,
         required : true
+    },
+    price: {
+        type : Number,
+        default : 0,
+
+    },
+    //CUANDO QUIERA AGREGAR UN  PRODUCTO USARE EL ID D ELA CATEGORIA, Y CREAREMOS EL ENLACE ENTRE AMBOS
+    //ESQUEMAS DE LASIGUIENTE FORMA, EN ESTE CASO EL LINK ES LA CATEGORIA
+    category: {
+        //AQUI HACEMOS REFERENCIA A QUE EL TIPO SERA DE UN ELEMENTO DENTRO DE UN ESQUEMA DE MONGOOSE
+        type : mongoose.Schema.Types.ObjectId,
+        //ANTERIORMENTE SE DIJO QUE USAREMOS EL ID PERO AUN NO ESPECIFICAMOS QUE
+        //ESQUEMA SERA ELQUE USAREMOS APRA ESTO SERA NECESARIO USAR UNA NEFERENCIA
+        ref : 'Category',
+        required :true 
+    },
+    countInShock: {
+        type : Number,
+        default : 0,
+        //PROPEIDAD ESPECIAL APRA ESTABLECER UN RANGO 
+        min: 0,
+        max: 255
+    },
+    rating : {
+        type : Number,
+        required : true
+    },
+    rating :{
+        type: Number,
+        default : 0
+    },
+    numReviews: {
+        type : Number,
+        default :  0
+    },
+    isFeature : {
+        type: Boolean,
+        default:  false
+    },
+    dateCreated : {
+        type : Date,
+        default : Date.now
     }
+
+
 })
 
 //LOS MODELOS SIEMPRE SE ESCRIBEN CON LETRA CAPITAL
