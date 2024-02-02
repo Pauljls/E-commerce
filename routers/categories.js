6127fedf367745969dc200808b6d410179d2e7d9
Pauljls <paulazarsol@gmail.com>
2024-02-02 17:17:13 -0500

    agregando funcion put y delete a category

--- routers/categories.js
+++ routers/categories.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const Category = require('../models/category')
-const category = require('../models/category')
 
 
 router.get('/',async(req,res)=>{
@@ -10,6 +9,18 @@ router.get('/',async(req,res)=>{
         res.status(500).json({sucess : false})
     }
     res.send(categoriesList);
+})
+
+//PEDIR DATOS POR ID, este nos devolvera solo un elemento
+router.get('/:id',async(req,res)=>{
+    const category = await Category.findById(req.params.id)
+    if(!category){
+    //con la fuincion json convertimos un objeto a json en este caso pasamos un objeto con algun contenido que querramos ver y
+    //lo convertimos a json para poder verlo por consola
+        return res.status(500).json({message: "La categoria con el ID brindado no existe"})
+    }
+
+    res.status(200).send(category);
 })
 
 router.post('/',async(req,res)=>{
@@ -35,6 +46,25 @@ router.post('/',async(req,res)=>{
 
     res.send(category);
 
+})
+
+router.put('/:id',async(req,res)=>{
+    const category = await Category.findByIdAndUpdate(req.params.id , {
+    //Para hacerlo mas sencillo podemos decir que re.body es
+    //requerimos del cuerpo en el frontend ya sea la vista o no,
+    //y leugo especificamos que aprte en el cuerpo ya se el nombre u otra cosa 
+        name : req.body.name,
+        icon : req.body.icon,
+        color : req.body.color
+
+    //SIEMPRE AL BUSCAR NOS DARA EL OBJETO ANTES DEL CAMBIO QUE DESEEMOS HACER ASI QUE
+    //AORA MOSTRARNOS EL OBEJTO CON LOS CAMBIOS DEBEMOS AGREGAR EL OBETO CON NEW    
+    }, {new :true})
+    if(!category){
+        return res.status(404).json({success : false , message : "No se encontro la categoria"})
+    }
+
+    res.send(category)
 })
 //api/v1/23151sad
 router.delete('/:id',(req,res)=>{
