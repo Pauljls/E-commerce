b71669f4afce731fb7181f27296867235dceb46d
Pauljls <paulazarsol@gmail.com>
2024-02-19 18:21:58 -0500

    JWT agregado y su explciacion en el servidor

--- routers/users.js
+++ routers/users.js
@@ -2,15 +2,17 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 router.get('/',async(req,res)=>{
 
-    const usersList = await User.find().select('name phone email')
+    const usersList = await User.find()//.select('name phone email')
     if(!usersList){
         res.status(500).json({sucess : false})
     }
     res.send(usersList);
 })
+
 router.get('/:id',async(req,res)=>{
     //CON SELECT AHREMOS UN FILTRADO COMO ANTERIORMENTE SE MENCIONO
     //EN ESTE CASO NO QUEREMOS QUE S EMUESTRE PASSWORD ASI QUE USAMOS -'SOMETHING'
@@ -47,6 +49,36 @@ router.post('/',(req,res)=>{
         return res.status(500).send(err)
     })
 
+})
+
+router.post('/login',async(req,res)=>{
+    const user  = await User.findOne({email : req.body.email})
+    if(!user){
+        return res.status(400).send('El usuario no pudo ser encontrado')
+    }
+    //DESPUES DE VERIFICAR QUE EL CORREO SE ENCUENTRA EN LA BASE DE DATOS NOS TOCARA
+    //REVISAR QUE LA CONTRASEÑA ES LA ADECUADA ADEMAS, PARA HACER ESTO USAREMOS BCRYPT
+    //PORQUE LA CONTRASEÑA FUE ANTERIORMENTE INCRIPTADA ASI QUE NECESITAMOS DESENCRIPTARLA
+    if(user && bcrypt.compareSync(req.body.password,user.passwordHash)){
+        
+        const secret = process.env.SECRET
+        const token = jwt.sign({
+            userId : user.id
+        },
+        //USAREMOS EL PARAMETRO SECRET COMO FIRMA Y DECIR QUE EL TOKEN ES VALIDO
+        //CON ESTO EL SERVIDOR SABE QUE EL TOKEN ES AUTENTICO Y NO ALGUIEN INTENTANDO HACER UNO FALSO
+        secret,
+        //ESTE APRAMETRO ES PARA OPCIONES EN ESTE CASO ELIGIRE EL TIMEPO DE EXPIRACION DEL TOKEN
+        {expiresIn : '1d'}) 
+
+        res.status(200).send({
+            name : user.email,
+            token : token
+        })
+    }else{
+        res.status(400).send('La contraseña es incorrecta')
+    }
+    
 })
 
 module.exports = router ;
