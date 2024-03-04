10adb94c9b95759642ecbcf7c98cef85a9876cd2
Pauljls <paulazarsol@gmail.com>
2024-03-04 17:25:27 -0500

    Agregando el modelo orden y ordenItem

--- routers/orders.js
+++ routers/orders.js
@@ -10,4 +10,26 @@ router.get('/',(req,res)=>{
     }
 })
 
+router.post('/',async(req,res)=>{
+    const order = new Order({
+        orderItems: req.body.orderItems,
+        shippingAddress1 : req.body.shippingAddress1, 
+        shippingAddress2 : req.body.shippingAddress2,
+        city : req.body.city,
+        zip : req.body.zip,
+        country : req.body.country,
+        phone : req.body.phone,
+        status : req.body.status,
+        totalPrice : req.body.totalPrice,
+        user : req.body.user
+    })
+    await order.save()
+
+    if(!order){
+        return res.status(400).send('La orden no puede ser creada')
+    }
+    res.send(order);
+
+})
+
 module.exports = router;
--- routers/users.js
+++ routers/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const user = require('../models/user')
+
 
 
 router.get('/', async(req,res)=>{
