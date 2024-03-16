ddb682d9021bf5a244310900742b2d3f16232fa0
Pauljls <paulazarsol@gmail.com>
2024-03-15 21:32:17 -0500

    Agregrando put y delete a nuestras rutas de ordenes

--- routers/orders.js
+++ routers/orders.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const Order = require('../models/order')
 const OrderItem = require('../models/order-item')
+const order = require('../models/order')
+const orderItem = require('../models/order-item')
 
 router.get('/',async(req,res)=>{
     //PODEMOS ESCOOGER EL NOMBRE DEL CAMPO QUE ES OTRO MODELO COMO PRIEMR PARAMETRO Y ADEMAS
@@ -45,7 +47,7 @@ router.post('/', async (req, res) => {
             return newOrderItem._id
         }));
 
-        console.log(orderItemsIds)
+        //console.log(orderItemsIds)
         //const orderItems = await Promise.all(orderItemsPromises);
 
         //FINALMENTE RESOLVEMOS LA PROMESA COMPBIANDA DE TODO EL ARRAY DE PROMESAS 
@@ -69,5 +71,32 @@ router.post('/', async (req, res) => {
         res.status(201).send(order);
 
 });
+
+router.put('/:id',async(req,res)=>{
+    let order = await Order.findByIdAndUpdate(req.params.id,{
+        status : req.body.status
+    },{new:true})
+    if(!order)
+    return res.status(400).send('La categoria no pudo ser actualziada')
+    res.status(200).send(order)
+})
+
+router.delete('/:id',(req,res)=>{
+    Order.findByIdAndDelete(req.params.id)
+    .then(  async order => {
+        if(order){
+            //UNA VEZ ELIMINAMOS EL OBJETO TENDREMOS QUE ELIMINAR LOS ELEMENTOS QUE SE GENERARON CON EL
+            //ENE ESTE CASO LOS ORDEITEMS, ESTOS SE PODRAN ELIMINAR COMOO SE CREARON MEDIANTE UN RECORRIDO
+            //USANDO MAP, ESTE SERA UNA OPERACION EN LA BD POR LO QUE TENDREMOS QUE USAR AWAIT POR CONSECUENTE
+            //SERA UNA FUNCION ANSINCRONICA EN SU TOTALIDAD
+            await order.orderItems.map(  async item => { await orderItem.findByIdAndDelete(item)} )
+            return res.status(200).send({success :  true ,  message : "La orden se elimino con exito"})
+        }else{
+            return res.status(400).send({success :  false, message: "La orden no existe"})
+        }
+    })
+    .catch(err => res.status(500).send({success :false,  message: err}))
+
+})
 
 module.exports = router;
